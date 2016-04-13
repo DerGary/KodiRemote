448a0c5d74a2ef4dee894a9740c8da3da825b1b3
Stefan Gerasch <stefangerasch1991@gmail.com>
2016-04-13 14:46:36 +0200

    Worked on Styles and Fonts for Home Control

--- KodiRemote/KodiRemote/View/HomePage.xaml.cs
+++ KodiRemote/KodiRemote/View/HomePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using KodiRemote.View.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -14,24 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
 namespace KodiRemote.View
 {
     /// <summary>
     /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
     /// </summary>
-    public sealed partial class MainPage : PageBase
+    public sealed partial class HomePage : Page
     {
-        public MainPage()
+        public HomePage()
         {
             this.InitializeComponent();
-            this.Loaded += MainPage_Loaded;
-        }
-
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            contentFrame.Navigate(typeof(HomePage));
         }
     }
 }
---
+++ KodiRemote/KodiRemote/View/UserControls/Home.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace KodiRemote.View.UserControls
+{
+    public sealed partial class Home : UserControl
+    {
+        public Home()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Image_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_ImageOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void itemGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
